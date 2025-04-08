d47a2451459a0ffbe4c3874f8288ea74f65d86ff
phantom2192005 <thinhph2192005@gmail.com>
2025-04-09 00:37:43 +0700

    ,

--- Assets/Scripts/Enemy/Behaviour/SteeringBehaviour.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class SteeringBehaviour : MonoBehaviour
-{
-    public abstract (float[] danger, float[] interest)
-        GetSteering(float[] danger, float[] interest, ContextData aiData);
-}
--- Assets/Scripts/UI/Model/StatModifier/CharacterStatModifierSO.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public abstract class CharacterStatModifierSO : ScriptableObject
-{
-    public abstract void AffectCharacter(GameObject target, float val);
-}
